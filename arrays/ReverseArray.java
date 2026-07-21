25864491947e91cb4d9cb5ff050cbecc177a3d89
saxy1973 <bhumikasaxena24112005@gmail.com>
2026-07-22 01:55:06 +0530

    array 7 code

--- arrays/ReverseArray.java
+++ arrays/ReverseArray.java
@@ -21,18 +21,21 @@ public class ReverseArray {
   }
   public static void main(String[] Args){
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the number of element");
+        System.out.print("Enter the number of element ");
         int n = sc.nextInt();
         int[] arr = new int[n];
         for(int i =0 ; i<n ; i++){
           arr[i] = sc.nextInt();
         }
         System.out.println("Orginal Array : ");
+        System.out.print("[ ");
          for(int i =0 ; i<n ; i++){
-          System.out.print("[ "+arr[i]+" ");
+          System.out.print(" "+arr[i]+" ");
         }
         System.out.print(" ]");
+        System.out.println();
         Reverse(arr);
+        sc.close();
 
  }
   
