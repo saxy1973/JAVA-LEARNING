2a67847cf357917f0aca64e243902823adf53a34
saxy1973 <bhumikasaxena24112005@gmail.com>
2026-07-22 02:10:14 +0530

    array 8 code

---
+++ arrays/MaxandMin.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class MaxandMin {
+
+    public static int findMax(int[] arr) {
+        int max = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        return max;
+    }
+
+    public static int findMin(int[] arr) {
+        int min = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+
+        return min;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Maximum Value = " + findMax(arr));
+        System.out.println("Minimum Value = " + findMin(arr));
+
+        sc.close();
+    }
+}
